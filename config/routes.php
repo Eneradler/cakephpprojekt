711681587c712af259185806772a29658ae91e6f
Bohdan Dakhno <bdakhno@gmail.com>
2018-01-22 04:47:48 +0100

    startseite modifiziert

--- config/routes.php
+++ config/routes.php
@@ -49,7 +49,7 @@ Router::scope('/', function (RouteBuilder $routes) {
      * its action called 'display', and we pass a param to select the view file
      * to use (in this case, src/Template/Pages/home.ctp)...
      */
-    $routes->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
+    $routes->connect('/', ['controller' => 'Jobs', 'action' => 'index']);
     //$routes->connect('/user', ['controller' => 'User', 'action' => 'index']);
     //$routes->connect('/userlist', ['controller' => 'User', 'action' => 'list']);
 
